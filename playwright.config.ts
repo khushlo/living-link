e50f106136970a0fa3117d2cf292d0e966a4e068
khushlo <khushlo@users.noreply.github.com>
2026-09-01 06:41:23 +0530

    fix: GIT pipeline had issue, removed Production build

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     },
   ],
   webServer: {
-    command: process.env.CI ? "npm run start" : "npm run dev",
+    command: "npm run dev",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
